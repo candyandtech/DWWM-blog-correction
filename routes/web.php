b042a49ecf682dd9d596c20c834e0d4b1f2ce16d
Jessica Z. <28598123+candyandtech@users.noreply.github.com>
2026-07-21 16:12:25 -0400

    feat: add route names for articles and admin articles management

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,17 @@ use App\Http\Controllers\ArticleController;
 Route::get('/', function () {
     return view('home');
 });
-Route::get('/articles', [ArticleController::class, 'index']);
+
+Route::get('/articles', [ArticleController::class, 'index'])->name('articles.index');
+// Route::get('/articles/{id}', [ArticleController::class, 'show'])->name('articles.show');
+
 Route::get('/admin/categories', [CategoryController::class, 'adminIndex']);
-Route::get('/admin/articles', [ArticleController::class, 'adminIndex']);
+
+// Route::middleware(['auth'])->group(function () {
+    Route::get('/admin/articles', [ArticleController::class, 'adminIndex'])->name('admin.articles.index');
+    Route::get('/admin/articles/create', [ArticleController::class, 'create'])->name('admin.articles.create');
+    Route::post('/admin/articles', [ArticleController::class, 'store'])->name('admin.articles.store');
+    Route::get('/admin/articles/{article}/edit', [ArticleController::class, 'edit'])->name('admin.articles.edit');
+    Route::put('/admin/articles/{article}', [ArticleController::class, 'update'])->name('admin.articles.update');
+    Route::delete('/admin/articles/{article}', [ArticleController::class, 'destroy'])->name('admin.articles.destroy');
+// });
