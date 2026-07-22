9ccf766d5c66224fc8c16df26daead03dbf9f968
Jessica Z. <28598123+candyandtech@users.noreply.github.com>
2026-07-22 09:53:09 -0400

    feat: add migration and update UserFactory and DatabaseSeeder to include firstname, lastname, and role columns

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,10 +15,19 @@ class DatabaseSeeder extends Seeder {
     public function run(): void {
         User::factory(10)->create();
 
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()->create([
+            'firstname' => 'Test',
+            'lastname' => 'Admin',
+            'email' => 'admin@example.com',
+            'role' => 'admin'
+        ]);
+
+        User::factory()->create([
+            'firstname' => 'Test',
+            'lastname' => 'User',
+            'email' => 'test@example.com',
+            'role' => 'user'
+        ]);
 
         $this->call([
             CategorySeeder::class,
