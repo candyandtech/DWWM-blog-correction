60e97576bc4bcdc967b882966d8178046a854f86
Jessica Z. <28598123+candyandtech@users.noreply.github.com>
2026-07-27 08:42:06 -0400

    feat: update User model fillable attributes to include firstname and lastname; comment out bulk user creation in DatabaseSeeder

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
-#[Fillable(['name', 'email', 'password'])]
+#[Fillable(['lastname', 'firstname', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable {
     /** @use HasFactory<UserFactory> */
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder {
      * Seed the application's database.
      */
     public function run(): void {
-        User::factory(10)->create();
+        // User::factory(10)->create();
 
         User::factory()->create([
             'firstname' => 'Test',
